21c5b35079af60a7f45a37c26ca7c278e8182e93
dbzfan2012 <ethank2784@gmail.com>
2025-04-25 16:54:20 -0700

    added some comments

--- hw3/fifo_ctrl.sv
+++ hw3/fifo_ctrl.sv
@@ -47,12 +47,14 @@ module fifo_ctrl #(parameter ADDR_WIDTH=4)
 		case ({rd, wr})
 			2'b11:  // read and write
 				begin
-					if (~empty) rd_ptr_next = rd_ptr + 1'b1;
-					if (~full) wr_ptr_next = wr_ptr + 2'b10;
-					if (rd_ptr_next == wr_ptr_next)
+					if (~empty) rd_ptr_next = rd_ptr + 1'b1; //Only reads if it's not empty
+					if (~full) wr_ptr_next = wr_ptr + 2'b10; //Only writes if it's not full
+					//If the next read and write pointers are equal, then it sets to full
+					//It will not have been empty or full before this, so it will also read and write
+					if (rd_ptr_next == wr_ptr_next) 
 							full_next = 1;
-					else	full_next = 0;
-					empty_next = 0;
+					else	full_next = 0; //Otherwise, it stays not full
+					empty_next = 0; //Never goes to empty after this
 				end
 			2'b10:  // read
 				if (~empty)
